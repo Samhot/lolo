bb5b704f5ea312dd716dacd92f7cc5a3e441cfc7
Thomas Punzano <thomas.punzano@inovatel.com>
2019-02-06 14:11:19 +0100

    added images and created all pages

--- demo/app/examples/basic/basic.component.ts
+++ demo/app/examples/basic/basic.component.ts
@@ -14,7 +14,11 @@ export class BasicExampleComponent implements OnInit {
     // this is just an example => for more details on config please visit fullPage.js docs
     this.config = {
       sectionsColor: ['#7BAABE', '#be5077', '#ffad76', '#ffae00', '#7BAABE'],
-      anchors: ['p1', 'p2', 'p3', 'p4', 'p5'],
+      anchors: [
+        'p1', 'p2', 'p3', 'p4', 'p5',
+        'p6', 'p7', 'p8', 'p9', 'p10',
+        'p11', 'p12', 'p13', 'p14', 'p15',
+        'p16', 'p17', 'p18', 'p19', 'p20'],
       navigation: true,
       scrollingSpeed: 700,
     };
